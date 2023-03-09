fbceb55a11d270a1c8fdab18b2a2212781f0c2b7
Dinesh9418 <67954716+Dinesh9418@users.noreply.github.com>
2023-03-09 08:49:12 +0000

    small changes

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountID = 9679;
+let accountEmail = "abc@gmail.com";
+var accountPassword = "Abc@123";
+let name;
+accountCity = "Jaipur";
+
+
+//accountID =2;
+accountEmail= "def@gmail.com"
+accountPassword = "Def@123"
+accountCity = "Beed"
+console.log([accountID, accountEmail, accountPassword, accountCity,name])
