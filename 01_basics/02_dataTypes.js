d5125afd7e162d66f7ddef956a2bc5178148718d
Dinesh9418 <67954716+Dinesh9418@users.noreply.github.com>
2023-03-10 10:54:55 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3); / we are using nodeJS not browser
+
+console.log(3 + 3);
+console.log("Dinesh")
+
+let name = "Dinesh"
+let age = 18
+let isLoggedIn = false
+
+//number  =>2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value
+let fName = null;
+//undefind 
+let state;
+//symbol => unique
+
+//object
+
+console.log(typeof (undefined)); //undefined
+console.log(typeof (null)); //object
