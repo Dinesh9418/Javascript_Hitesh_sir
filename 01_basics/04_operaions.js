2e3b0a50a8a883d9e7c708ce40652e86de381e56
Dinesh9418 <67954716+Dinesh9418@users.noreply.github.com>
2023-03-12 12:40:08 +0000

    Basics Operations

---
+++ 01_basics/04_operaions.js
@@ -0,0 +1,30 @@
+let value = 3
+let negValue = -value
+console.log(negValue)
+console.log("Addiion " + (2 + 2));
+console.log("Subsrtacion " + 2 - 2);
+console.log("Divide " + 2 / 2);
+console.log("Muliple " + 2 * 2);
+console.log("Power " + 2 ** 2);
+console.log("Mod " + 2 % 2);
+
+let str1 = "Dinesh"
+let str2 = " Jadhav"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(("2" + 2));
+console.log((1 + 2 + "2"));
+
+console.log(true);
+
+let num1, num2, num3;
+
+num1 = num2=num3 = 2+2;
+console.log(num1, num2, num3);
+
+let gameCouner =100;
+++gameCouner;
+console.log(gameCouner);
+
