851ff58ec00ffa2b5a76c3d17758e0a487946948
Dinesh9418 <67954716+Dinesh9418@users.noreply.github.com>
2023-03-14 17:49:32 +0000

    math and number

---
+++ 01_basics/08_NumberAndMath.js
@@ -0,0 +1,28 @@
+const score = 400
+//console.log(score);
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(typeof (balance.toString()));
+//console.log(balance.toFixed(3));
+
+const otherNumbre = 231.740
+//console.log(otherNumbre.toPrecision(3));
+
+const NumberOne = 100000
+//console.log(NumberOne.toLocaleString('en-IN'));
+
+//+++++++++++++++MaTHS+++++++++++++++++++++
+
+// console.log(Math);
+console.log(Math.abs(-2));
+console.log(Math.round(4.59));
+console.log(Math.ceil(4.5));
+console.log(Math.floor(4.5));
+console.log(Math.min(1, -2, 3, 2, 1, 4));
+console.log(Math.max(1, 2, 3, 2, 1, 4));
+console.log((Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
