b5e13cee3af608977beef612168e00938d4fc33e
Dinesh9418 <67954716+Dinesh9418@users.noreply.github.com>
2023-03-14 06:28:00 +0000

    Stak and heap memory

--- 01_basics/06_DataTypes_summary.js
+++ 01_basics/06_DataTypes_summary.js
@@ -5,37 +5,58 @@ const score = 100
 const scoreValue = 1000.3
 
 const isLoggedIn = false
-console.log("isLogged In " +typeof isLoggedIn)
+//console.log("isLogged In " +typeof isLoggedIn)
 
 const outSideTemp = null
-console.log("outSide Temp " +typeof outSideTemp)
-
+//console.log("outSide Temp " +typeof outSideTemp)
 
 let userEmail;
 
 const id = Symbol('123');
 const anotherID = Symbol('123')
-console.log(id === anotherID);
+//console.log(id === anotherID);
 
 const bigNumber = 12345678910;
-console.log(typeof bigNumber)
+//console.log(typeof bigNumber)
 
 /* Reference Type / non-primitive
 Array , Objects, Functions
 */
 
-
 const heros = ["shaktiman", "naagraj", "doga"]
-console.log(typeof heros)
+//console.log(typeof heros)
 
 let myObj = {
     name: "Dinesh",
     age: 22
 }
-console.log(typeof myObj)
+//console.log(typeof myObj)
 
 const myFunction = function () {
-    console.log("Hello world");
+    //  console.log("Hello world");
 }
+//console.log(typeof myFunction)
 
-console.log(typeof myFunction)
+//Memory
+// 1)Stack (primitive) 
+
+//2) Heap (non-primitive)
+
+let myYoutubeName = "LTIMindtree"
+let anotherName = myYoutubeName;
+
+anotherName = "Scrobits"
+
+console.log(myYoutubeName)
+console.log(anotherName);
+
+let user1 = {
+    email: "user@gmail.com",
+    upi: "user@icici"
+}
+console.log(user1.email);
+
+let userTwo = user1;
+userTwo.email = "Dinesh@gmail.com"
+console.log(user1.email);
+console.log(userTwo.email);
